8a30b468af8bf017968d528a67145d7416fc78cf
matanamar82 <amarmatan2021@gmail.com>
2024-03-17 01:35:30 +0200

    added pinMode reducer

--- routes/api.js
+++ routes/api.js
@@ -5,9 +5,10 @@ router.get('/:point', async function(req, res) {
     const point = req.params.point
     console.log(point);
     try{
+        // `https://api.opentopodata.org/v1/aster30m?locations=${point}`
         const response = await fetch(`https://api.opentopodata.org/v1/aster30m?locations=${point}`);
         const data = await response.json();
-        //console.log(`the data is: ${data.data}`)
+        console.log(`the data is: ${data.data}\n\n`)
         res.json({ data });
     }
     catch{
